933e741c95d2ccb7e6ce667c38cdea7e3ea7550c
Farukul Islam <farukgb1999@gmail.com>
2023-10-08 00:10:23 +0600

    navbar style done

--- src/Component/Shared/Navbar/Navbar.jsx
+++ src/Component/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
 const Navbar = () => {
+  const menuItem = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+      <li>
+        <a>Teachers</a>
+      </li>
+      <li>
+        <a className="primaryBgColor cursor-pointer">Login</a>
+      </li>
+    </>
+  );
   return (
-    <div>
-      <div className="navbar primaryBgColor">
-        <div className="navbar-start">
+    <div className="primaryBgColor">
+      <div className="navbar my-container">
+        <div className="">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
@@ -24,54 +40,14 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow primaryBgColor rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menuItem}
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+        <div className="hidden mx-auto lg:flex">
+          <ul className="menu font-bold menu-horizontal px-1">{menuItem}</ul>
         </div>
       </div>
     </div>
