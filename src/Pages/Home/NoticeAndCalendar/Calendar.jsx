dca3e50a02878e864e385f9b4ed33c7014fd05a1
Farukul Islam <farukgb1999@gmail.com>
2023-10-12 23:26:23 +0600

    calendar and noticboard make

---
+++ src/Pages/Home/NoticeAndCalendar/Calendar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Calendar = () => {
+  return <div>This is calendar section</div>;
+};
+
+export default Calendar;
---
+++ src/Pages/Home/NoticeAndCalendar/NoticeBoard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NoticeBoard = () => {
+  return <div>This is noticeBoard</div>;
+};
+
+export default NoticeBoard;
