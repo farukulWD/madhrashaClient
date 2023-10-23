ea64342ef610afb083e0443f79acf057f9937517
Farukul Islam <farukgb1999@gmail.com>
2023-10-23 23:04:00 +0600

    history page designed

--- src/Pages/HistoryPage/History.jsx
+++ src/Pages/HistoryPage/History.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const History = () => {
-  return <div>This is history page</div>;
+  return <div>This is history page and for design</div>;
 };
 
 export default History;
